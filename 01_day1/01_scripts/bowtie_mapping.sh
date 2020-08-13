7a66e41108c89367bda1ff0dd8291fb136fb7fae
atigano <atigano@users.noreply.github.com>
2020-08-13 14:06:55 -0400

    Create bowtie_mapping.sh

---
+++ 01_day1/01_scripts/bowtie_mapping.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+###bowtie_mapping.sh 
+
+SAMPLELIST=$1 ### samplelist_all.txt
+
+cd ~/home/physalia_adaptation_course/01_day1/02_genome/
+bowtie2-build -f genome_mallotus_dummy.fasta mallotus
+
+mkdir ~/home/mapped/
+cd ~/home/mapped/
+mkdir samfiles
+mkdir bamfiles
+
+for IND in `cat $SAMPLELIST`; do
+bowtie2 -p 24 -q --phred33 --end-to-end --very-sensitive --fr --time -x ~/home/physalia_adaptation_course/01_day1/02_genome/mallotus -U ~/home/data/${IND}.fq.gz -S ~/home/mapped/samfiles/${IND}.sam
+
+cd ~/home/mapped/samfiles/
+### to filter multiple alignments
+grep -v XS:i: ${IND}.sam > ${IND}.xs.sam
+samtools view -bS ${IND}.xs.sam > ../bamfiles/${IND}.bam
+
+done
+
+cd ../
+rm -rf samfiles
