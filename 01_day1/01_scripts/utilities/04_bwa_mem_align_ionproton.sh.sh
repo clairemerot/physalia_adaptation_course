7d0b587fc555936c2af8d12d508eb0d46a316da5
Claire Merot <claire.merot@gmail.com>
2020-08-14 14:04:11 -0400

    added alignment scripts

---
+++ 01_day1/01_scripts/utilities/04_bwa_mem_align_ionproton.sh.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH -J "GBS_Align"
+#SBATCH -o log_%j
+#SBATCH -c 4 
+#SBATCH -p medium
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=claire.merot@gmail.com
+#SBATCH --time=7-00:00
+#SBATCH --mem=10G
+
+# Important: Move to directory where job was submitted
+cd $SLURM_SUBMIT_DIR
+
+#this script is adapted from stacks workflow https://github.com/enormandeau/stacks_workflow by Eric Normandeau
+#it is tailored for single-end reads coming from ionproton sequencing
+
+# Global variables
+GENOMEFOLDER="02_genome"
+GENOME="genome_mallotus_dummy.fasta"
+DATAFOLDER="03_raw_reads"
+ALIGNEDFOLDER="04_aligned_files"
+NCPU=$4
+
+# Test if user specified a number of CPUs
+if [[ -z "$NCPU" ]]
+then
+    NCPU=1
+fi
+
+# Load needed modules
+module load bwa
+module load samtools
+
+# Index genome if not alread done
+bwa index -p "$GENOMEFOLDER"/"$GENOME" "$GENOMEFOLDER"/"$GENOME"
+
+for file in $(ls -1 "$DATAFOLDER"/*.fa.gz)
+do
+    # Name of uncompressed file
+    echo "Aligning file $file"
+
+    name=$(basename "$file")
+    ID="@RG\tID:ind\tSM:ind\tPL:IonProton"
+
+    # Align reads 1 step
+    bwa mem -t "$NCPU" -k 19 -c 500 -T 0 -E 2,2 \
+        -R "$ID" \
+        "$GENOMEFOLDER"/"$GENOME" "$DATAFOLDER"/"$name" 2> /dev/null |
+        samtools view -Sb -q 1 -F 4 -F 256 -F 2048 \
+        - > "$ALIGNEDFOLDER"/"${name%.fa.gz}".bam
+
+    # Samtools sort
+    samtools sort --threads "$NCPU" -o "$ALIGNEDFOLDER"/"$name".sorted.bam "$ALIGNEDFOLDER"/"${name%.fa.gz}".bam
+
+    # Cleanup
+    rm "$ALIGNEDFOLDER"/"${name%.fa.gz}".bam
+done
+
+
