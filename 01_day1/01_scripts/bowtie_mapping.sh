b74a197efe18fabc517ccb9678201d7e909bbcd3
atigano <atigano@users.noreply.github.com>
2020-08-17 11:54:49 -0400

    Update bowtie_mapping.sh

--- 01_day1/01_scripts/bowtie_mapping.sh
+++ 01_day1/01_scripts/bowtie_mapping.sh
@@ -3,18 +3,18 @@
 
 SAMPLELIST=$1 ### samplelist_all.txt
 
-cd ~/home/physalia_adaptation_course/01_day1/02_genome/
+cd ~/Anna/scripts
 bowtie2-build -f genome_mallotus_dummy.fasta mallotus
 
-mkdir -p ~/home/mapped/
-cd ~/home/mapped/
+mkdir -p ~/Anna/mapped/
+cd ~/Anna/mapped/
 mkdir -p samfiles
 mkdir -p bamfiles
 
 for IND in `cat $SAMPLELIST`; do
-bowtie2 -p 24 -q --phred33 --end-to-end --very-sensitive --fr --time -x ~/home/physalia_adaptation_course/01_day1/02_genome/mallotus -q ~/home/data/${IND}.fq.gz > ~/home/mapped/samfiles/${IND}.sam >2 bowtie.log
+bowtie2 -p 24 -q --phred33 --end-to-end --very-sensitive --fr --time -x ~/Anna/scripts/mallotus -q ~/Anna/data/${IND}.fq.gz > ~/Anna/mapped/samfiles/${IND}.sam >2 bowtie.log
 
-cd ~/home/mapped/samfiles/
+cd ~/Anna/mapped/samfiles/
 grep -v XS:i: ${IND}.sam > ${IND}.xs.sam
 samtools view -b ${IND}.xs.sam | samtools sort - > ../bamfiles/${IND}.bam
 
