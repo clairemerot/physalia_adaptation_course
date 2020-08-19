b2654e44336a755dec92e4ce70f7a9937362c71f
atigano <atigano@users.noreply.github.com>
2020-08-19 13:48:56 -0400

    Update stacks_gstacks.sh

--- 01_day1/01_scripts/stacks_gstacks.sh
+++ 01_day1/01_scripts/stacks_gstacks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ###stacks_gstacks.sh
+cd
 mkdir -p stacks
 cd stacks
 mkdir -p gstacks
-cd ~/home/
-gstacks -I ~/home/mapped/bamfiles -M physalia_adaptation_course/00_documents/popmap_all.txt -O gstacks -t 24
+gstacks -I ~/mapped/bamfiles -M ~/scripts/popmap_all.txt -O gstacks -t 3
