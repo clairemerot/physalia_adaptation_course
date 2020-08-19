1a29c82a6a789b73873f890ebff80f297308b95b
Claire Merot <claire.merot@gmail.com>
2020-08-19 17:20:51 -0400

    update alignement script

--- 01_day1/01_scripts/utilities/04_bwa_mem_align_ionproton.sh.sh
+++ 01_day1/01_scripts/utilities/04_bwa_mem_align_ionproton.sh.sh
@@ -4,28 +4,27 @@
 #SBATCH -c 4 
 #SBATCH -p medium
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=claire.merot@gmail.com
+#SBATCH --mail-user=XXX@gmail.com
 #SBATCH --time=7-00:00
 #SBATCH --mem=10G
 
-# Important: Move to directory where job was submitted
+# Important: Move to directory where job was submitted -> this is useful for slurm. not is run directly
 cd $SLURM_SUBMIT_DIR
 
 #this script is adapted from stacks workflow https://github.com/enormandeau/stacks_workflow by Eric Normandeau
 #it is tailored for single-end reads coming from ionproton sequencing
+#use a different script for paired-end data and different parameters according to the sequencing platform
+
+
 
 # Global variables
 GENOMEFOLDER="02_genome"
 GENOME="genome_mallotus_dummy.fasta"
 DATAFOLDER="03_raw_reads"
 ALIGNEDFOLDER="04_aligned_files"
-NCPU=$4
+NCPU=4 #should match what is asked to slurm in the header
 
-# Test if user specified a number of CPUs
-if [[ -z "$NCPU" ]]
-then
-    NCPU=1
-fi
+
 
 # Load needed modules
 module load bwa
@@ -43,14 +42,14 @@ do
     ID="@RG\tID:ind\tSM:ind\tPL:IonProton"
 
     # Align reads 1 step
-    bwa mem -t "$NCPU" -k 19 -c 500 -O 0,0 -E 2,2 -T 0 \
+    bwa mem -t "$NCPU" -k 19 -c 500 -T 0 -E 2,2 \
         -R "$ID" \
         "$GENOMEFOLDER"/"$GENOME" "$DATAFOLDER"/"$name" 2> /dev/null |
         samtools view -Sb -q 1 -F 4 -F 256 -F 2048 \
         - > "$ALIGNEDFOLDER"/"${name%.fq.gz}".bam
 
     # Samtools sort
-    samtools sort --threads "$NCPU" -o "$ALIGNEDFOLDER"/"$name".sorted.bam "$ALIGNEDFOLDER"/"${name%.fq.gz}".bam
+    samtools sort --threads "$NCPU" -o "$ALIGNEDFOLDER"/"${name%.fq.gz}".sorted.bam "$ALIGNEDFOLDER"/"${name%.fq.gz}".bam
 
     # Cleanup
     rm "$ALIGNEDFOLDER"/"${name%.fq.gz}".bam
