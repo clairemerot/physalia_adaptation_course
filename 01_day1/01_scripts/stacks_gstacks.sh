f1bf5c6fbd0cedba68aea0d95ddc28ae9c520104
atigano <atigano@users.noreply.github.com>
2020-09-08 10:52:47 -0400

    Update stacks_gstacks.sh

--- 01_day1/01_scripts/stacks_gstacks.sh
+++ 01_day1/01_scripts/stacks_gstacks.sh
@@ -4,4 +4,4 @@ cd
 mkdir -p stacks
 cd stacks
 mkdir -p gstacks
-gstacks -I ~/mapped/bamfiles -M ~/scripts/popmap_all.txt -O gstacks -t 3
+gstacks -I ~/bamfiles -M ~/scripts/popmap_all.txt -O gstacks -t 3
